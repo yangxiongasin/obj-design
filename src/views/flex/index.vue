76d8f67c2d05ad47b71da95b56240bcc2f8f21ec
zx-PC\zx <zhangxin26@asiainfo.com>
2019-06-13 14:23:30 +0800

    1

--- src/views/flex/index.vue
+++ src/views/flex/index.vue
@@ -20,14 +20,29 @@ export default {
           `<div class="d-flex"><h1></h1><p></p></div>`
         ],
         [
-          'd-flex-inline',
-          ' 行内弹性盒子',
-          `<div class="d-flex-inline"><h1></h1><p></p></div>`
+          'justify-content-center',
+          ' 居中水平对齐',
+          `<div class="justify-content-center"><h1></h1><p></p></div>`
         ],
         [
-          'd-flex-row',
-          ' 水平排列',
-          `<div class="d-flex-row"><h1></h1><p></p></div>`
+          'justify-content-between',
+          ' 俩端不留白水平对齐',
+          `<div class="justify-content-between"><h1></h1><p></p></div>`
+        ],
+        [
+          'justify-content-around',
+          ' 俩端留白水平对齐',
+          `<div class="justify-content-around"><h1></h1><p></p></div>`
+        ],
+        [
+          'align-items-center',
+          ' 在元素中间位置垂直对齐',
+          `<div class="align-items-center d-flex"><h1></h1><p></p></div>`
+        ],
+        [
+          'justify-align-center',
+          '元素水平垂直居中对齐',
+          `<div class="justify-align-center"><h1></h1><p></p></div>`
         ],
         [
           'd-flex-row-reverse',
@@ -55,21 +70,6 @@ export default {
           `<div class="justify-content-end"><h1></h1><p></p></div>`
         ],
         [
-          'justify-content-center',
-          ' 居中水平对齐',
-          `<div class="justify-content-center"><h1></h1><p></p></div>`
-        ],
-        [
-          'justify-content-between',
-          ' 俩端不留白水平对齐',
-          `<div class="justify-content-between"><h1></h1><p></p></div>`
-        ],
-        [
-          'justify-content-around',
-          ' 俩端留白水平对齐',
-          `<div class="justify-content-around"><h1></h1><p></p></div>`
-        ],
-        [
           'align-items-start',
           ' 在元素开始位置垂直对齐',
           `<div class="align-items-start d-flex"><h1></h1><p></p></div>`
@@ -79,11 +79,7 @@ export default {
           ' 在元素末尾位置垂直对齐',
           `<div class="align-items-end d-flex"><h1></h1><p></p></div>`
         ],
-        [
-          'align-items-center',
-          ' 在元素中间位置垂直对齐',
-          `<div class="align-items-center d-flex"><h1></h1><p></p></div>`
-        ],
+
         [
           'align-items-baseline',
           ' 在元素基线位置垂直对齐(元素中的文本都以第一个元素的文本的基线对齐)',
@@ -118,11 +114,6 @@ export default {
           'align-self-stretch',
           ' 默认值-子元素被拉伸以适应容器',
           `<div class="d-flex"><h1 class="align-self-stretch"></h1><p class="align-self-stretch"></p></div>`
-        ],
-        [
-          'justify-align-center',
-          '元素水平垂直居中对齐',
-          `<div class="justify-align-center"><h1></h1><p></p></div>`
         ]
       ]
     }
