410b685233f53fa7666a2d220c783b8d712463b5
yangxiong1234 <1908825931@qq.com>
2019-06-13 11:47:00 +0800

    1

--- src/views/sizing/index.vue
+++ src/views/sizing/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="d-flex">
-    sizing
+  <div style="width: 500px; height: 250px;" class="bg-blue-100">
+    <div class="w-25 bg-blue-400 h-25"  style="background-color: rgba(0,0,255,.1)">25%</div>
+    <div class="w-50 bg-blue-400 h-25"  style="background-color: rgba(0,0,255,.1)">25%</div>
+    <div class="w-75 bg-blue-400 h-25"  style="background-color: rgba(0,0,255,.1)">25%</div>
+    <div class="w-100 bg-blue-400 h-25"  style="background-color: rgba(0,0,255,.1)">25%</div>
   </div>
 </template>
 
