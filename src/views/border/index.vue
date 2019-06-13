0d2f7b959965d2ecd653ca72597b76a0a4fd4221
luzq <1037336039@qq.com>
2019-06-13 16:31:35 +0800

    update border

--- src/views/border/index.vue
+++ src/views/border/index.vue
@@ -1,14 +1,91 @@
 <template>
-  <div class="d-flex">
-    border
+  <div class="text-16">
+    <div class="mt-24 mb-24">项目中所用边框颜色为E8E8E8；此表格只做样式展示，所用边框颜色为#CCCCCC</div>
+    <div
+      class="d-flex position"
+    >
+      <obj-table
+        :thead-data="headerData"
+        :tbody-data="bodyData"
+      ></obj-table>
+    </div>
+    <!--<div></div>-->
+    <!--<div-->
+      <!--class="d-flex position"-->
+    <!--&gt;-->
+      <!--<obj-table-->
+        <!--:thead-data="headerData"-->
+        <!--:tbody-data="bodyData"-->
+      <!--&gt;</obj-table>-->
+    <!--</div>-->
   </div>
 </template>
 
 <script>
   export default {
-    name: 'border',
+    name: 'position-list',
     data () {
-      return {}
+      return {
+        headerData: ['类名', '描述', '示例'],
+        bodyData: [
+          [
+            'border-outline',
+            '通用边框',
+            `<p class="border-outline" style="width: 50px; height: 50px; border-color: #CCCCCC"></p>`
+          ],
+          [
+            'border-0',
+            '无边框',
+            `<p class="border-0 bg-lighter" style="width: 50px; height: 50px; border-color: #CCCCCC"></p>`
+          ],
+          [
+            'border-left',
+            '只含左边框',
+            `<p class=" border-left bg-lighter" style="width: 50px; height: 50px; border-color: #CCCCCC"></p>`
+          ],
+          [
+            'border-top',
+            '只含上边框',
+            `<p class=" border-top bg-lighter" style="width: 50px; height: 50px; border-color: #CCCCCC"></p>`
+          ],
+          [
+            'border-right',
+            '只含右边框',
+            `<p class=" border-right bg-lighter" style="width: 50px; height: 50px; border-color: #CCCCCC"></p>`
+          ],
+          [
+            'border-bottom',
+            '只含下边框',
+            `<p class=" border-bottom bg-lighter" style="width: 50px; height: 50px"></p>`
+          ],
+          [
+            'border-left-0',
+            '缺左边框',
+            `<p class=" border-left-0 bg-lighter" style="width: 50px; height: 50px"></p>`
+          ],
+          [
+            'border-top-0',
+            '缺上边框',
+            `<p class=" border-top-0 bg-lighter" style="width: 50px; height: 50px"></p>`
+          ],
+          [
+            'border-right-0',
+            '缺右边框',
+            `<p class=" border-right-0 bg-lighter" style="width: 50px; height: 50px"></p>`
+          ],
+          [
+            'border-bottom-0',
+            '缺下边框',
+            `<p class=" border-bottom-0 bg-lighter" style="width: 50px; height: 50px"></p>`
+          ],
+        ]
+      }
     }
   }
 </script>
+<style>
+  .position table p {
+    width: 10px;
+    height: 10px;
+  }
+</style>
--- src/views/color/index.vue
+++ src/views/color/index.vue
@@ -35,7 +35,7 @@
       <h1 class="title">渐变背景</h1>
       <div class="d-flex justify-content-around">
         <div
-          v-for="item in colorShador"
+          v-for="item in colorShadow"
           :key="item.value"
           :class="[item.className, 'item']">
           {{item.className}}
@@ -52,19 +52,19 @@ export default {
     return {
       bgColorGreys: [
         {
-          className: 'bg-gray-1',
+          className: 'bg-lighter',
           value: '#F8F9FB',
           text: '(用法：纯色背景)'
         }, {
-          className: 'bg-gray-2',
+          className: 'bg-light',
           value: '#828282',
           text: '(用法：列表悬停色和纯色背景)'
         }, {
-          className: 'bg-gray-3',
+          className: 'bg-medium',
           value: '#E8E8E8',
           text: '(用法：线、分割符和禁用图标)'
         }, {
-          className: 'bg-gray-4',
+          className: 'bg-dark',
           value: '#E1E4E8',
           text: '(用法：文本输入框和禁用背景)'
         }
@@ -104,7 +104,7 @@ export default {
           text: '(用法：禁用文本。输入标签)'
         }
       ],
-      colorShador: [
+      colorShadow: [
         {
           className: 'gradient-blue',
           text: '(用法：纯色背景)'
@@ -122,264 +122,6 @@ export default {
           text: '(用法：文本输入框和禁用背景)'
         }
       ],
-      colorTextList_0: [
-        {
-          color: 'color-black-900',
-          bgColor: 'bg-black-900',
-          borderColor: 'border-black-900',
-          value: '#0A1F44'
-        }, {
-          color: 'color-black-800',
-          bgColor: 'bg-black-800',
-          borderColor: 'border-black-800',
-          value: '#182C4F'
-        }, {
-          color: 'color-black-700',
-          bgColor: 'bg-black-700',
-          borderColor: 'border-black-700',
-          value: '#273A5B'
-        }, {
-          color: 'color-black-600',
-          bgColor: 'bg-black-600',
-          borderColor: 'border-black-600',
-          value: '#364866'
-        }, {
-          color: 'color-black-500',
-          bgColor: 'bg-black-500',
-          borderColor: 'border-black-500',
-          value: '#445571'
-        }, {
-          color: 'color-black-400',
-          bgColor: 'bg-black-400',
-          borderColor: 'border-black-400',
-          value: '#53627C'
-        }, {
-          color: 'color-black-300',
-          bgColor: 'bg-black-300',
-          borderColor: 'border-black-300',
-          value: '#5D6C84'
-        }, {
-          color: 'color-black-200',
-          bgColor: 'bg-black-200',
-          borderColor: 'border-black-200',
-          value: '#6C798F'
-        }, {
-          color: 'color-black-100',
-          bgColor: 'bg-black-100',
-          borderColor: 'border-black-100',
-          value: '#758196'
-        }
-      ],
-      colorTextList_1: [
-        {
-          color: 'color-gray-900',
-          bgColor: 'bg-gray-900',
-          value: '#8A94A6'
-        }, {
-          color: 'color-gray-800',
-          bgColor: 'bg-gray-800',
-          value: '#98A1B1'
-        }, {
-          color: 'color-gray-700',
-          bgColor: 'bg-gray-700',
-          value: '#A6AEBC'
-        }, {
-          color: 'color-gray-600',
-          bgColor: 'bg-gray-600',
-          value: '#B5BBC6'
-        }, {
-          color: 'color-gray-500',
-          bgColor: 'bg-gray-500',
-          value: '#C9CED6'
-        }, {
-          color: 'color-gray-400',
-          bgColor: 'bg-gray-400',
-          value: '#E1E4E8'
-        }, {
-          color: 'color-gray-300',
-          bgColor: 'bg-gray-300',
-          value: '#F1F2F4'
-        }, {
-          color: 'color-gray-200',
-          bgColor: 'bg-gray-200',
-          value: '#F8F9FB'
-        }, {
-          color: 'color-gray-100',
-          bgColor: 'bg-gray-100',
-          value: '#FCFCFD'
-        }
-      ],
-      colorTextList_2: [
-        {
-          color: 'color-black-navigation',
-          bgColor: 'bg-black-navigation',
-          value: '#15161A'
-        }, {
-          color: 'color-black-default',
-          bgColor: 'bg-black-default',
-          value: '#212434'
-        }, {
-          color: 'color-navigation-hover',
-          bgColor: 'bg-navigation-hover',
-          value: '#2B2D39'
-        }
-      ],
-      colorTextList_3: [
-        {
-          color: 'color-blue-900',
-          bgColor: 'bg-blue-900',
-          value: '#0A44A5'
-        }, {
-          color: 'color-blue-800',
-          bgColor: 'bg-blue-800',
-          value: '#0D55CF'
-        }, {
-          color: 'color-blue-700',
-          bgColor: 'bg-blue-700',
-          value: '#0C66FF'
-        }, {
-          color: 'color-blue-600',
-          bgColor: 'bg-blue-600',
-          value: '#007FFF'
-        }, {
-          color: 'color-blue-500',
-          bgColor: 'bg-blue-500',
-          value: '#5AACFF'
-        }, {
-          color: 'color-blue-400',
-          bgColor: 'bg-blue-400',
-          value: '#99CCFF'
-        }, {
-          color: 'color-blue-300',
-          bgColor: 'bg-blue-300',
-          value: '#CCE6FF'
-        }, {
-          color: 'color-blue-200',
-          bgColor: 'bg-blue-200',
-          value: '#EBF5FF'
-        }, {
-          color: 'color-blue-100',
-          bgColor: 'bg-blue-100',
-          value: '#F5FAFF'
-        }
-      ],
-      colorTextList_4: [
-        {
-          color: 'color-green-900',
-          bgColor: 'bg-green-900',
-          value: '#006242'
-        }, {
-          color: 'color-green-800',
-          bgColor: 'bg-green-800',
-          value: '#00865A'
-        }, {
-          color: 'color-green-700',
-          bgColor: 'bg-green-700',
-          value: '#0BB07B'
-        }, {
-          color: 'color-green-600',
-          bgColor: 'bg-green-600',
-          value: '#22C993'
-        }, {
-          color: 'color-green-500',
-          bgColor: 'bg-green-500',
-          value: '#3ED3A3'
-        }, {
-          color: 'color-green-400',
-          bgColor: 'bg-green-400',
-          value: '#6DDFBA'
-        }, {
-          color: 'color-green-300',
-          bgColor: 'bg-green-300',
-          value: '#92ECCF'
-        }, {
-          color: 'color-green-200',
-          bgColor: 'bg-green-200',
-          value: '#CFF8EB'
-        }, {
-          color: 'color-green-100',
-          bgColor: 'bg-green-100',
-          value: '#EAFCF7'
-        }
-      ],
-      colorTextList_5: [
-        {
-          color: 'color-red-900',
-          bgColor: 'bg-red-900',
-          value: '#A50000'
-        }, {
-          color: 'color-red-800',
-          bgColor: 'bg-red-800',
-          value: '#BD0303'
-        }, {
-          color: 'color-red-700',
-          bgColor: 'bg-red-700',
-          value: '#DD2727'
-        }, {
-          color: 'color-red-600',
-          bgColor: 'bg-red-600',
-          value: '#F03D3D'
-        }, {
-          color: 'color-red-500',
-          bgColor: 'bg-red-500',
-          value: '#FF5D5D'
-        }, {
-          color: 'color-red-400',
-          bgColor: 'bg-red-400',
-          value: '#FF8D8D'
-        }, {
-          color: 'color-red-300',
-          bgColor: 'bg-red-300',
-          value: '#FFC2C2'
-        }, {
-          color: 'color-red-200',
-          bgColor: 'bg-red-200',
-          value: '#FFEAEA'
-        }, {
-          color: 'color-red-100',
-          bgColor: 'bg-red-100',
-          value: '#FFF8F8'
-        }
-      ],
-      colorTextList_6: [
-        {
-          color: 'color-yellow-900',
-          bgColor: 'bg-yellow-900',
-          value: '#F07300'
-        }, {
-          color: 'color-yellow-800',
-          bgColor: 'bg-yellow-800',
-          value: '#FF8F00'
-        }, {
-          color: 'color-yellow-700',
-          bgColor: 'bg-yellow-700',
-          value: '#FFAD0D'
-        }, {
-          color: 'color-yellow-600',
-          bgColor: 'bg-yellow-600',
-          value: '#FFBB38'
-        }, {
-          color: 'color-yellow-500',
-          bgColor: 'bg-yellow-500',
-          value: '#FFCA65'
-        }, {
-          color: 'color-yellow-400',
-          bgColor: 'bg-yellow-400',
-          value: '#FFDC99'
-        }, {
-          color: 'color-yellow-300',
-          bgColor: 'bg-yellow-300',
-          value: '#FFECC7'
-        }, {
-          color: 'color-yellow-200',
-          bgColor: 'bg-yellow-200',
-          value: '#FFF6E4'
-        }, {
-          color: 'color-yellow-100',
-          bgColor: 'bg-yellow-100',
-          value: '#FFFCF5'
-        }
-      ]
     }
   }
 }
