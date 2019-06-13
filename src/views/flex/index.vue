74b669822276c4cac964dac0c0892cc47dff0599
zx-PC\zx <zhangxin26@asiainfo.com>
2019-06-13 17:27:38 +0800

    eslint

--- src/views/flex/index.vue
+++ src/views/flex/index.vue
@@ -25,19 +25,63 @@ export default {
       bodyData: [
         ['d-flex', ' 一个使用display:flex的容器', `<p>display:flex</p>`],
         ['flex-wrap', ' 换行', `<p>display: flex;</p><p>flex-wrap: wrap;</p>`],
-        ['flex-ju-center', ' 居中水平对齐', `<p>display: flex;</p><p>justify-content: center;</p>`],
-        ['flex-ju-between', ' 俩端不留白水平对齐', `<p>display: flex;</p><p>justify-content: space-between;</p>`],
-        ['flex-al-center', ' 在元素中间位置垂直对齐', `<p>display: flex;</p><p>align-items: center;</p>`],
-        ['flex-ju-al-center', '元素水平垂直居中对齐', `<p>display: flex;</p><p>align-items: center;</p><p>justify-content: center;</p>`],
-        ['flex-ju-al-between', '元素水平两端，垂直居中对齐', `<p>display: flex;</p><p>align-items: center;</p><p>justify-content: space-between.flex p;</p>`],
-        ['d-flex-column', ' 垂直排列', `<p>display: flex;</p><p>flex-direction: column;</p>`],
+        [
+          'flex-ju-center',
+          ' 居中水平对齐',
+          `<p>display: flex;</p><p>justify-content: center;</p>`
+        ],
+        [
+          'flex-ju-between',
+          ' 俩端不留白水平对齐',
+          `<p>display: flex;</p><p>justify-content: space-between;</p>`
+        ],
+        [
+          'flex-al-center',
+          ' 在元素中间位置垂直对齐',
+          `<p>display: flex;</p><p>align-items: center;</p>`
+        ],
+        [
+          'flex-ju-al-center',
+          '元素水平垂直居中对齐',
+          `<p>display: flex;</p><p>align-items: center;</p><p>justify-content: center;</p>`
+        ],
+        [
+          'flex-ju-al-between',
+          '元素水平两端，垂直居中对齐',
+          `<p>display: flex;</p><p>align-items: center;</p><p>justify-content: space-between.flex p;</p>`
+        ],
+        [
+          'd-flex-column',
+          ' 垂直排列',
+          `<p>display: flex;</p><p>flex-direction: column;</p>`
+        ]
       ],
       bodyData2: [
-        ['d-flex-row-reverse', ' 水平反序排列', `<p>display: flex;</p><p>flex-direction: row-reverse;</p>`],
-        ['flex-ju-start', ' 在元素开始位置水平对齐', `<p>display: flex;</p><p>justify-content: flex-start;</p>`],
-        ['flex-ju-end', ' 在元素末尾位置水平对齐', `<p>display: flex;</p><p>justify-content: flex-end;</p>`],
-        ['flex-al-start', ' 在元素开始位置垂直对齐', `<p>display: flex;</p><p>align-items: flex-start;</p>`],
-        ['flex-al-end', ' 在元素末尾位置垂直对齐', `<p>display: flex;</p><p>align-items: flex-end;</p>`],
+        [
+          'd-flex-row-reverse',
+          ' 水平反序排列',
+          `<p>display: flex;</p><p>flex-direction: row-reverse;</p>`
+        ],
+        [
+          'flex-ju-start',
+          ' 在元素开始位置水平对齐',
+          `<p>display: flex;</p><p>justify-content: flex-start;</p>`
+        ],
+        [
+          'flex-ju-end',
+          ' 在元素末尾位置水平对齐',
+          `<p>display: flex;</p><p>justify-content: flex-end;</p>`
+        ],
+        [
+          'flex-al-start',
+          ' 在元素开始位置垂直对齐',
+          `<p>display: flex;</p><p>align-items: flex-start;</p>`
+        ],
+        [
+          'flex-al-end',
+          ' 在元素末尾位置垂直对齐',
+          `<p>display: flex;</p><p>align-items: flex-end;</p>`
+        ]
       ]
     }
   }
