bd5fce2f0c53386dfb47f0276670669d0636d5aa
yangxiong1234 <1908825931@qq.com>
2019-06-18 09:29:52 +0800

    1

--- src/components/objLeftTree/index.vue
+++ src/components/objLeftTree/index.vue
@@ -1,62 +1,140 @@
 <template>
   <!-- 懒加载、手风琴、多选框 -->
-  <el-tree
-  :props="treeProps"
-  :load="$com_loadNode"
-  node-key="id"
-  lazy
-  accordion
-  show-checkbox
+  <el-tree v-if="showTree"
+  ref="leftTree" :props="treeProps" :load="$com_loadNode"
+  node-key="id" :expand-on-click-node="false"
+  lazy accordion :show-checkbox="showCheckbox"
   :render-content="$com_renderContent"
-  @node-click="$com_searchTree"
-  @check-change="$com_checkChange"
-  >
+  @node-click="$com_clickTree"
+  @check-change="$com_checkChange">
   </el-tree>
 </template>
 <script>
 export default {
+  props: {
+    http_treedata: {
+      type: Function
+    },
+    showCheckbox: {
+      type: Boolean,
+      default: true
+    },
+    showIcon: {
+      type: Boolean,
+      default: true
+    },
+    uniformColor: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
   data () {
     return {
+      showTree: this.value,
       treeProps: {
         label: 'name', // 指定节点标签为节点对象的某个属性值
-        children: 'childs' // 指定子树为节点对象的某个属性值
-        // disabled: 'zones', // 指定节点选择框是否禁用为节点对象的某个属性值
-        // isLeaf: 'leaf' // 是否为叶子节点
+        children: 'childs', // 指定子树为节点对象的某个属性值
+        isLeaf: (data, node) => {
+          if (data.leaf === '1') return true
+          return false
+        } // 是否为叶子节点
+      },
+      parentId: ''
+    }
+  },
+  watch: {
+    value (val) {
+      this.showTree = val
+    }
+  },
+  computed: {
+    params () {
+      return {
+        parentId: this.parentId
       }
     }
   },
   methods: {
-    addTreeNode (func) {
-      const that = this
-      that.$http.post(that.$service.listtree, {
-        parentId: ''
-      }).then(res => {
-        console.log(res)
-        let data = res.data.data
-        if (!func) {
-          // that.treeData = data
-          if (res.data.data.length <= 0) return
-          this.$emit('getOrgCode', res.data.data[0].orgCode)
-        } else {
-          func(data || [])
-        }
-      })
-    },
+    // 判断当前节点是否包含子节点
     $com_loadNode (node, resolve) {
       const that = this
-      that.addTreeNode(resolve)
+      if (node.isLeaf) return
+      if (node.data) this.parentId = node.data.id
+      if (typeof this.http_treedata === 'function') {
+        that.http_treedata.call(this, resolve, that.params)
+      }
     },
     // 传递给 data 属性的数组中该节点所对应的对象、节点本身是否被选中、节点的子树中是否有被选中的节点
     $com_checkChange (data, checked, indeterminate) {
-      console.log(data, checked, indeterminate)
+      const datas = this.$refs.leftTree.getCheckedNodes()
+      this.$emit('checkData', datas)
     },
+    // render自定义样式渲染
     $com_renderContent (h, { node, data, store }) {
-      return h('span', [
-        h('span', {}, node.label + 'yangxiong')
-      ])
+      const that = this
+      let icontooltip = ''
+      // 如果是叶子类目并且是禁用状态
+      if (node.isLeaf && data.online === '0') {
+        icontooltip = '未上线'
+      } else if (node.isLeaf && data.status === '0') {
+        icontooltip = '已禁用'
+      }
+      let icon = h(
+        'el-tooltip',
+        {
+          props: {
+            effect: 'dark',
+            placement: 'top',
+            content: icontooltip
+          }
+        },
+        [
+          h('span', {
+            class: {
+              'color-alert': node.isLeaf && data.online === '0',
+              'color-error': node.isLeaf && data.status === '0',
+              'icon-erp-alert-triangle': node.isLeaf && data.status === '0',
+              'icon-erp-eye-off': node.isLeaf && data.online === '0',
+              'text-18': true,
+              'cursor-not-allowed': node.isLeaf && data.status === '0',
+              'mr-4': true
+            }
+          })
+        ]
+      )
+      if (!that.showIcon) { icon = '' }
+      return h(
+        'div',
+        {
+          class: {
+            'flex-ju-al-center': true
+          }
+        },
+        [
+          icon,
+          h(
+            'span',
+            {
+              class: {
+                'color-light': !node.isLeaf || that.uniformColor,
+                'color-medium': node.isLeaf && !that.uniformColor,
+                'color-input': node.isLeaf && data.status === '0' && !that.uniformColor,
+                'text-14': true,
+                'cursor-not-allowed': node.isLeaf && data.status === '0'
+              }
+            },
+            node.label
+          )
+        ]
+      )
     },
-    $com_searchTree () {
-      return 1
+    // 传递给 data 属性的数组中该节点所对应的对象、节点对应的 Node、节点组件本身。
+    $com_clickTree (data, node, store) {
+      this.$emit('getCurrentId', data.id)
     }
   }
 }
--- src/views/leftTree/index.vue
+++ src/views/leftTree/index.vue
@@ -1,6 +1,46 @@
 <template>
   <div>
-    <obj-left-tree></obj-left-tree>
-    <div class="icon-erp-activity"></div>
+    <obj-left-tree
+    :value="demo"
+    :show-checkbox="false"
+    :show-icon="false"
+    :uniform-color="true"
+    :http_treedata="$http_treeData"
+    @getCurrentId="$com_currentId"
+    @checkData="$com_checkData"></obj-left-tree>
+    <el-button @click="demo1"></el-button>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      currentId: '',
+      demo: true
+    }
+  },
+  methods: {
+    demo1 () {
+      this.demo = false
+      this.$nextTick(() => {
+        this.demo = true
+      })
+    },
+    $http_treeData (func, option) {
+      const that = this
+      that.$http.post(that.$service.listtree, option).then(res => {
+        let data = res.data.data
+        if (res.data.data.length <= 0) { return false }
+        func(data || [])
+      })
+    },
+    $com_currentId (id) {
+      this.currentId = id
+    },
+    $com_checkData (data) {
+      console.log(data, 'demo')
+    }
+  }
+}
+</script>
