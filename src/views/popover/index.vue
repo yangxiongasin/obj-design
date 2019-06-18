812608100201be344d3f887f6c23f781c265fdbc
luzq <1037336039@qq.com>
2019-06-18 14:01:38 +0800

    update popover 完成

--- src/views/popover/index.vue
+++ src/views/popover/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="popover">
     <h3 class="mt-20 mb-8">popover 类型</h3>
-    <h4 class="mt-16 mb-16">注意：触发 popover 插入 slot 时，需修改 slot 名：原 elementUI 的 popover 组件值 reference 改为 ocj_reference </h4>
+    <h4 class="mt-16 mb-16 color-light">注意：触发 popover 插入 slot 时，需修改 slot 名：原 elementUI 的 popover 组件值 reference 改为 ocj_reference </h4>
     <div class="d-flex flex-ju-bet">
-      <div class="mr-20 code">
+      <code class="pr-40 mr-40 border-right">
         <pre class="color-primary text-16">{{ typeCode }}</pre>
-      </div>
+      </code>
       <obj-popover
         ocj-title="属性启用"
         ocj-content="是否确认启用属性？"
@@ -16,35 +16,38 @@
       </obj-popover>
     </div>
     <h3 class="mt-20 mb-8">自行插入slot内容</h3>
-    <h4 class="mt-16 mb-16">注意：内容区域插入 slot 时，需带上 slot 名：ocj_content</h4>
+    <h4 class="mt-16 mb-16 color-light">注意：内容区域插入 slot 时，需带上 slot 名：ocj_content</h4>
     <div class="d-flex flex-ju-bet">
-      <div class="mr-20 code">
+      <code class="pr-40 mr-40 border-right">
         <pre class="color-primary text-16">{{ slotCode }}</pre>
-      </div>
+      </code>
       <obj-popover
         type="error">
         <div slot="ocj_content">自行插入slot的内容</div>
         <el-button slot="ocj_reference">click 激活</el-button>
       </obj-popover>
     </div>
-
-    <h3 class="mt-20 mb-8">新增属性</h3>
-    <h4 class="mt-16 mb-16">说明：如下所列中修改原 elementUI 中 popover 组件 trigger、title、content、width 四个参数；新增 show-footer、show-cancel-btn、type、show-icon 等四个参数</h4>
-    <div class="d-flex border">
-      <obj-table
-        :thead-data="headerData"
-        :tbody-data="radiusData"
-      ></obj-table>
-    </div>
-    <h3 class="mt-20 mb-8">新增事件</h3>
-    <h4 class="mt-16 mb-16">说明：支持原elementUI的所有事件的基础上新增底部按钮的两个点击事件的回传：ok 和 cancel</h4>
-    <!--<div class="text-16 mb-8 text-weight-bold"></div>-->
-    <div class="d-flex border">
-      <obj-table
-        :thead-data="eventsHeader"
-        :tbody-data="eventsData"
-      ></obj-table>
-    </div>
+    <el-tabs v-model="activeName" class="mt-40">
+      <el-tab-pane label="属性" name="first">
+        <h4 class="mt-16 mb-16 color-light">说明：如下所列中修改原 elementUI 中 popover 组件 trigger、title、content、width 四个参数；新增 show-footer、show-cancel-btn、type、show-icon 等四个参数</h4>
+        <div class="d-flex border">
+          <obj-table
+            :thead-data="headerData"
+            :tbody-data="radiusData"
+          ></obj-table>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="事件" name="second">
+        <h4 class="mt-16 mb-16 color-light">说明：支持原elementUI的所有事件的基础上新增底部按钮的两个点击事件的回传：ok 和 cancel</h4>
+        <!--<div class="text-16 mb-8 text-weight-bold"></div>-->
+        <div class="d-flex border">
+          <obj-table
+            :thead-data="eventsHeader"
+            :tbody-data="eventsData"
+          ></obj-table>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 <script>
@@ -53,6 +56,7 @@ export default{
     return {
       obj_visible: false,
       obj_visible1: false,
+      activeName: 'first',
       showFooter: true,
       showCancelBtn: true,
       showIcon: true,
